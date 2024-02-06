2f5b7df46363fd6ede9a2e12456f93808fa1b797
+ <+>
2024-02-06 12:52:15 +0300

    Completed Assignment

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,24 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div>
+      <h1>Directors Page</h1>
+      {directors.map((director, index) => (
+        <div key={index}>
+          <h1>{director.name}</h1>
+          <ul>
+            <h3>Movies starred in:</h3>
+            {director.movies.map((movie, index) => (
+              <li key={index}>
+                {movie}
+              </li>
+            ))}
+          </ul>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Directors;
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,33 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom/cjs/react-router-dom.min";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+    <div className="navbar">
+      <NavLink
+        to="/"
+        exact
+      >
+        Home
+      </NavLink>
+      <NavLink
+        to="/movies"
+        exactS
+      >
+        Movies
+      </NavLink>
+      <NavLink
+        to="/actors"
+      >
+        Actors
+      </NavLink>
+      <NavLink
+      to="/directors"
+      exact
+      >
+        Directors
+      </NavLink>
+    </div>
+  );
 }
-
 export default NavBar;
